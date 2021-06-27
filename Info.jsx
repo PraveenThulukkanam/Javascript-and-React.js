87036ed05a6ec38e5ba3260379462cf38b49e8bf
PraveenThulukkanam <86544185+PraveenThulukkanam@users.noreply.github.com>
2021-06-27 10:23:32 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This is an amazing basic web development bootcamp on javascript and
+        React js taken up by Sir Shaurya Sinha . We learned the basics of HTML ,
+        Javascript and React.js from scratch !!!
+      </p>
+    </div>
+  );
+}
+
+export default Info;
